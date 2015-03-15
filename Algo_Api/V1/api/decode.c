117818065251830ce7f8ab8e08cd5ec63babdcbf
fanqh <fqh1100@163.com>
2015-03-15 18:06:59 +0800

    save work

--- Algo_Api/V1/api/decode.c
+++ Algo_Api/V1/api/decode.c
@@ -8,7 +8,7 @@
 //NRVIBO 欧瑞博 曼切斯特码协议
 #define   SYN_L		(700-100)/TIME_UNIT
 #define   SYN_H		(700+100)/TIME_UNIT
-#define   SINGLE_L  (5)/TIME_UNIT
+#define   SINGLE_L  1//(5)/TIME_UNIT
 #define   SINGLE_H  (175+60)/TIME_UNIT
 #define   DOUBLE_L  (250)/TIME_UNIT	    
 #define   DOUBLE_H  (350+230)/TIME_UNIT
@@ -51,13 +51,33 @@ static uint16 RF_decode(RF_AC_DATA_TYPE *pdata, GPIO_TypeDef* GPIOx, uint16_t GP
 	uint16 TimeHigh = 0;  //高脉冲长度
 	uint16 TimeLow = 0;	  //低脉冲长度
 	uint16 Timebase = 0;
+     uint16     Head = 0;
 
+	sucode = 0;
+	time = 0;
 	p = pdata->buff;
 
     Enable_SysTick();		//启动定时器0
 	__disable_irq();
 	TimeCount = 0;
     __enable_irq();
+
+//	if(!GPIO_ReadInputDataBit( GPIOx,  GPIO_Pin))
+//	{
+		while(!(GPIO_ReadInputDataBit(GPIOx,  GPIO_Pin))) 
+		{
+			if(TimeCount > WIDE_MAX)  //128T
+				return 0;
+			
+		}
+		Head = Get_TimeCount_CleanAndStart(); 
+		if((Head>75/TIME_UNIT)&&(Head<250/TIME_UNIT))
+		{
+		}
+		else
+			return 0;
+//		printf("Head = %d\r\n", Head);
+//	}
 	 
 	while(GPIO_ReadInputDataBit( GPIOx,  GPIO_Pin))	   //检测同步脉冲
 	{
@@ -73,9 +93,10 @@ static uint16 RF_decode(RF_AC_DATA_TYPE *pdata, GPIO_TypeDef* GPIOx, uint16_t GP
 	}
 	wide = Get_TimeCount_CleanAndStart();
 
-//	printf("narrow = %d， wide = %d\r\n", narrow, wide);
+	//printf("narrow = %d， wide = %d\r\n", narrow, wide);
 	if(((narrow>NRROW_MIN )&& (narrow*24)<wide) && (wide<(narrow*38)))  //narrow >20 为过LV
-	{
+	{  
+#if 0
 		Timebase = pdata->TimeBase = (wide + narrow) / 32;     // T
 		for(ii=0; ii<LEN_MAX; ii++)
 		{
@@ -133,15 +154,13 @@ static uint16 RF_decode(RF_AC_DATA_TYPE *pdata, GPIO_TypeDef* GPIOx, uint16_t GP
 #endif       	
 	    	}
 			++p;
-		}	
+		}
+#endif	
 	}
-	else if((narrow>SYN_L)&&(narrow<SYN_H)&&(wide>SINGLE_L)&&(TIME_UNIT<SINGLE_H))
+	else if((narrow>SYN_L)&&(narrow<SYN_H)&&(wide>150/TIME_UNIT)&&(wide<250/TIME_UNIT)) //(Head>75/TIME_UNIT)&&(Head<250/TIME_UNIT)&&
 	{
-		sucode = 0;
-		time = 0;
-
-
-//		printf("head is ok  %d\r\n", sucode);
+		printf("narrow = %d, wide = %d\r\n", narrow*5, wide*5);
+		printf("**start anylysis orvibo**\r\n\r\n");
 		for(ii=0; ii<4; ii++)
 		{
 			for(k=9; k>0; k--)
@@ -154,33 +173,55 @@ static uint16 RF_decode(RF_AC_DATA_TYPE *pdata, GPIO_TypeDef* GPIOx, uint16_t GP
 						while(GPIO_ReadInputDataBit( GPIOx,  GPIO_Pin))	   //检测1 脉冲长度
 						{
 							if(TimeCount > DOUBLE_H)	  //4T
+							{   
+							    printf("erro: sucode: 0, GPIO: 1, >DOUBLE_H, sucode= %d, 1. timecount is %d, i= %d, k= %d\r\n\r\n",sucode,time*5, ii,k);
+								//printf("erro: sucode: 0, GPIO: 1, >DOUBLE_H\r\n\r\n");
+								BSP_mDelay(1000);
 								return 0;
+							}
+								
 						}
 						time = Get_TimeCount_CleanAndStart();							
 
-						printf("sucode= %d, 1. timecount is %d, i= %d, k= %d\r\n",sucode,time*5, ii,k);
+						//printf("sucode= %d, 1. timecount is %d, i= %d, k= %d\r\n",sucode,time*5, ii,k);
 					    if((time>SINGLE_L)&&(time<SINGLE_H))
 						{
 							sucode = 1;	
 						}
 						else
+						{
+							printf("erro: sucode: 0, GPIO: 1, other err, sucode= %d, 1. timecount is %d, i= %d, k= %d\r\n\r\n",sucode,time*5, ii,k);
+						//	printf("erro: sucode: 0, GPIO: 1, other err\r\n\r\n");
+						BSP_mDelay(1000);
 							return 0;
+						}
 					}
 				    else
 					{
 						while(!GPIO_ReadInputDataBit( GPIOx,  GPIO_Pin))	   //检测1 脉冲长度
 						{
 							if(TimeCount > DOUBLE_H)	  //4T
+							{
+								printf("erro: sucode: 0, GPIO: 0, >DOUBLE_H, sucode= %d,  0. timecount is %d,i= %d, k= %d\r\n\r\n", sucode,time*5,ii,k);
+							//	printf("erro: sucode: 0, GPIO: 0, >DOUBLE_H\r\n\r\n");
+							BSP_mDelay(1000);
 								return 0;
+							}
+								
 						}
 						time = Get_TimeCount_CleanAndStart();	
-						printf("sucode= %d,  0. timecount is %d,i= %d, k= %d\r\n",sucode,time*5,ii,k);
+						//printf("sucode= %d,  0. timecount is %d,i= %d, k= %d\r\n",sucode,time*5,ii,k);
 					    if((time>SINGLE_L)&&(time<SINGLE_H))
 						{
 							sucode = 1;	
 						}
 						else
+						{
+							printf("erro: sucode: 0, GPIO: 0, other err, sucode= %d, 0. timecount is %d, i= %d, k= %d\r\n\r\n",sucode,time*5, ii,k);
+						//	printf("erro: sucode: 0, GPIO: 0, other err\r\n\r\n");
+						BSP_mDelay(1000);
 							return 0;	
+						}
 					}
 				}
 				else if(sucode==1)
@@ -191,38 +232,57 @@ static uint16 RF_decode(RF_AC_DATA_TYPE *pdata, GPIO_TypeDef* GPIOx, uint16_t GP
 						while(GPIO_ReadInputDataBit( GPIOx,  GPIO_Pin))	   //检测1 脉冲长度
 						{
 							if(TimeCount > DOUBLE_H)	  //4T
+							{
+								printf("erro: sucode: 1, GPIO: 1, >DOUBLE_H, sucode= %d,  1. timecount is %d, i= %d, k= %d\r\n\r\n",sucode, time*5,ii,k);
+							//	printf("erro: sucode: 1, GPIO: 1, >DOUBLE_H\r\n\r\n");
+							BSP_mDelay(1000);
 								return 0;
+							}
 						}
 						time = Get_TimeCount_CleanAndStart();	
-						printf("sucode= %d,  1. timecount is %d, i= %d, k= %d\r\n",sucode,time*5,ii,k);
+						//printf("sucode= %d,  1. timecount is %d, i= %d, k= %d\r\n",sucode, time*5,ii,k);
+
 					    if((time>SINGLE_L)&&(time<SINGLE_H))
 						{
 							sucode = 0;	
 							RF_ORVIBO_RECE[ii] &= ~(1UL<<ii);
+							//printf("bit = 0\r\n");
 
 						}
 						else if((time>DOUBLE_L)&&(time<DOUBLE_H))
 						{
 							sucode = 1;
 							RF_ORVIBO_RECE[ii] &= ((1UL<<ii));
+							//printf("sucode =1, bit = 0\r\n");
 						}
 						else
+						{
+							printf("erro: sucode: 1, GPIO: 1, other err\r\n\r\n, sucode= %d,  1. timecount is %d, i= %d, k= %d\r\n\r\n",sucode, time*5,ii,k);
+						//	printf("erro: sucode: 1, GPIO: 1, other err\r\n\r\n");
+						BSP_mDelay(1000);
 							return 0;
+						}
 					}
 				    else
 					{
 						Get_TimeCount_CleanAndStart();
 						while(!GPIO_ReadInputDataBit( GPIOx,  GPIO_Pin))	   //检测1 脉冲长度
 						{
-							if(TimeCount > DOUBLE_H)	  //4T
+							if(TimeCount >= DOUBLE_H)	  //4T
+							{
+								printf("erro: sucode: 1, GPIO: 1, >DOUBLE_H, sucode= %d,  0. timecount is %d,i= %d, k= %d\r\n\r\n",sucode,time*5,ii,k);
+							//	printf("erro: sucode: 1, GPIO: 1, >DOUBLE_H\r\n\r\n");
+							BSP_mDelay(1000);
 								return 0;
+							}
 						}
 						time = Get_TimeCount_CleanAndStart();	
-						printf("sucode= %d,  1. timecount is %d,i= %d, k= %d\r\n",sucode,time*5,ii,k);
+					//	printf("sucode= %d,  0. timecount is %d,i= %d, k= %d\r\n",sucode,time*5,ii,k);
 					    if((time>SINGLE_L)&&(time<SINGLE_H))
 						{
 							sucode = 0;	
 							RF_ORVIBO_RECE[ii] |= (1UL<<ii);
+							//
 
 						}
 						else if((time>DOUBLE_L)&&(time<DOUBLE_H))
@@ -231,16 +291,21 @@ static uint16 RF_decode(RF_AC_DATA_TYPE *pdata, GPIO_TypeDef* GPIOx, uint16_t GP
 							RF_ORVIBO_RECE[ii] |= (1UL<<ii);
 						}
 						else
+						{
+							printf("erro: sucode: 1, GPIO: 0, other err, sucode= %d,  0. timecount is %d,i= %d, k= %d\r\n\r\n",sucode,time*5,ii,k);
+							//printf("erro: sucode: 1, GPIO: 0, other err\r\n");
+							BSP_mDelay(1000);
 							return 0;	
+						}
 					}		
 				}
 					
 			}
 		}
-		printf("receive RC800 is ok\r\n");
+	//	printf("receive RC800 is ok\r\n");
 		return 0;	
 	}
-	return 0;
+		return 0;
 }
 
 
